bdfa72860d9db27b8b59d86302ace807fce0c386
SummitFlow Dev <kasadis@summitflow.dev>
2026-03-16 22:53:40 +0000

    delete stale dev-standards-config.sh wrapper

--- scripts/lib/dev-standards-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Thin wrapper to the canonical SummitFlow dev-standards-config.sh.
-CANONICAL_SUMMITFLOW_ROOT="${SUMMITFLOW_BACKUP_ROOT:-$HOME/summitflow}"
-source "$CANONICAL_SUMMITFLOW_ROOT/scripts/lib/dev-standards-config.sh"
